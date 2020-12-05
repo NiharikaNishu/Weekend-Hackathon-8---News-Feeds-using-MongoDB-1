8b5fdabde068f6164a11b07f0756a1bc978d989e
Lakkakula Venkatesh <lakkakula.venkatesh8726@gmail.com>
2020-12-05 18:18:48 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 8080
-
-const onePageArticleCount = 10
+const express = require('express');
+const app = express();
+const port = 8080;
+const {newsArticleModel} = require("./connector");
+const onePageArticleCount = 10;
 
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get('/newFeeds/:limit?/:offset?', (req, res) => {
+    // console.log(req.params.limit + " " + req.params.offset);
+    let docCount = ((req.params.limit === undefined) || isNaN(req.params.limit)) ? onePageArticleCount : parseInt(req.params.limit);
+    let skipCount = ((req.params.offset === undefined) || isNaN(req.params.limit)) ? 0 : parseInt(req.params.offset);
+    newsArticleModel.find().limit(docCount).skip(skipCount).then(dailynews => res.status(200).send(dailynews));
+    return;
+});
 
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
