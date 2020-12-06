ce88197a85c32022337b29c98dd5a1023a4768ae
Lakkakula Venkatesh <lakkakula.venkatesh8726@gmail.com>
2020-12-06 16:37:58 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get('/newFeeds/', (req, res) => {
-    let limit = (!req.query.limit || isNaN(req.query.limit)) ? onePageArticleCount : parseInt(req.query.limit);
+    let limit = (!req.query.limit || isNaN(req.query.limit)) ? 10 : parseInt(req.query.limit);
     let offset = (!req.query.offset || isNaN(req.query.limit)) ? 0 : parseInt(req.query.offset);
     newsArticleModel.find().limit(limit).skip(offset).then(dailynews => res.status(200).send(dailynews));
     return;
